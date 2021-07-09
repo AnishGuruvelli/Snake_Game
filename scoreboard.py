f19c6f07b448b04da8abe5bc53cf791b29f8b9db
anish_2418 <78360526+AnishGuruvelli@users.noreply.github.com>
2021-07-09 15:12:31 +0530

    updated the high score feature in the game

--- scoreboard.py
+++ scoreboard.py
@@ -24,6 +24,7 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.penup()
         self.color("white")
         self.goto(0, 270)
@@ -32,17 +33,23 @@ class Score(Turtle):
         self.update_score()
 
     def update_score(self):
-        self.write(f"Score : {self.score} ", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score : {self.score} High Score : {self.high_score} ", align=ALIGNMENT, font=FONT)
         # but when we do this,the turtle is present there somewhere in the middle, so we want that to disappear sorted()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
 
-
+    # commenting out these to make a few changes like, displaying the high score and still continuing with the game
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
+        # self.clear()
         self.update_score()
 
